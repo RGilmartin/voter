f7aa7113adf1788e331ffd19cc8256c495a1664a
Ryan Gilmartin <ryan22gil@gmail.com>
2023-10-03 00:15:25 -0400

    working version without authentication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
     import { getAuth } from 'firebase/auth';
 	import { getStorage } from 'firebase/storage';
     import 'charts.css';
-	import internal from 'stream';
 
     // Initialize Firebase
     const firebaseConfig = {
@@ -26,30 +25,119 @@
 
 <FirebaseApp {auth} {firestore} {storage}>
     <slot />
-    <h1>hello</h1>
-    <Doc ref="questions/dogcat" let:data>
-        <h2>{data.title}</h2>
-        <div class="chart">
-            <p>{data.Dog}</p>
-            <p>{data.option1.name}: {data.option1.value}</p>
-            <table class="charts-css column multiple show-heading show-labels show-primary-axis show-data-axes show-2-secondary-axes show-5-secondary-axes">
-                <thead>
-                    <tr>
-                        <th scope="col">{data.option1.name}</th>
-                        <th scope="col">{data.option2.name}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>{data.option1.name}</th>
-                        <td style="--size: calc( {data.option1.value / (data.option1.value + data.option2.value)} )">{(parseInt(data.option1.value) / (parseInt(data.option1.value) + parseInt(data.option2.value))).toString()}</td>
-                    </tr>
-                    <tr>
-                        <th>{data.option2.name}</th>
-                        <td style="--size: .5">4</td>
-                    </tr>
-                </tbody>
-            </table> 
+
+    <div class="page">
+        <Doc ref="questions/dogcat" let:data>
+            <h2>{data.title}</h2>
+            <div class="chart">
+                <table id="dataChart" class="charts-css column multiple show-heading show-labels datasets-spacing-7">
+                    <thead>
+                        <tr>
+                            <th scope="col"><p>{data.option1.name}</p></th>
+                            <th scope="col"><p>{data.option2.name}</p></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>{data.option1.name}</th>
+                            <td style="--size: {data.option1.value/(parseInt(data.option1.value) + parseInt(data.option2.value))}"><p>{data.option1.value}</p></td>
+                        </tr>
+                        <tr>
+                            <th>{data.option2.name}</th>
+                            <td style="--size: {data.option2.value/(parseInt(data.option1.value) + parseInt(data.option2.value))}"><p>{data.option2.value}</p></td>
+                        </tr>
+                    </tbody>
+                </table> 
+            </div>
+        </Doc>
+
+        <div id="qr-show">
+            <div id="qr-text">
+                <h3>Want to Vote?</h3>
+                <h5>Scan this qr code and it will take you where you want to go.</h5>
+            </div>
+            <div id="qr-code">
+                <img src="./scan_qr.svg" alt="qr code to access voting">
+            </div>
         </div>
-    </Doc>
+    </div>
 </FirebaseApp>
+
+<style>
+    .page {
+        margin: -8px;
+        padding: 0;
+        color: #fafafa;
+        font-family: Arial, Helvetica, sans-serif;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: start;
+        flex-direction: column;
+        height: 100vh;
+        width: 100vw;
+        background-color: salmon;
+    }
+
+    #dataChart td{
+        background-color: white;
+        border-radius: 5px;
+    }
+
+    #dataChart td p {
+        margin-top: -3rem;
+        font-size: 2.8rem;
+        font-weight: bold;
+    }
+
+    #dataChart tbody th {
+        font-size: 1.5rem;
+        margin-top: 84%;
+        font-weight: bold;
+    }
+
+    h2 {
+        font-size: 5rem;
+        padding-left: 4rem;
+    }
+
+    .chart {
+        width: 35%;
+        align-self: center;
+    }
+
+    #qr-show {
+        height: 15rem;
+        width: 30rem;
+        background-color: darkslategrey;
+        align-self: center;
+        margin-top: 7rem;
+        border-radius: 10px;
+        border: 3px solid rgb(26, 44, 44);
+        display: flex;
+        text-align: center;
+    }
+    #qr-text {
+        width: 50%;
+        text-align: left;
+        padding: 20px 10px 0px 10px;
+    }
+
+    #qr-text h3 {
+        font-weight: bold;
+        font-size: 1.9rem;
+    }
+
+    #qr-text h5 {
+        font-weight: 400;
+        font-size: .9rem;
+    }
+
+    #qr-code {
+        width: 50%;
+    }
+    
+    #qr-code img {
+        height: 90%;
+        padding-top: 5%;
+    }
+</style>
---
+++ src/routes/vote/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+    import { Doc, FirebaseApp, docStore } from 'sveltefire';
+    import { initializeApp } from 'firebase/app';
+    import { updateDoc, setDoc, getFirestore, DocumentReference } from 'firebase/firestore';
+    import { getAuth } from 'firebase/auth';
+	import { getStorage } from 'firebase/storage';
+    import { goto } from '$app/navigation';
+    import 'charts.css';
+
+    // Initialize Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyDGpRbJyuhM1t3F5zxUM3neDgG69tZyIP8",
+        authDomain: "office-vote.firebaseapp.com",
+        databaseURL: "https://office-vote-default-rtdb.firebaseio.com",
+        projectId: "office-vote",
+        storageBucket: "office-vote.appspot.com",
+        messagingSenderId: "946893048079",
+        appId: "1:946893048079:web:1b31b6df09a5bd30bad67a"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const firestore = getFirestore(app);
+    const auth = getAuth(app);
+    const storage = getStorage(app);
+
+    const refer = "questions/dogcat";
+</script>
+
+<FirebaseApp {firestore} {auth} {storage}>
+    <Doc ref="{refer}" let:data let:ref let:firestore>
+        <div id="page">
+            <div id="title">
+                <h1>{data.title}</h1>
+            </div>
+            {#if ref != null}
+            <div id="bttn-wrap">
+                <button on:click={() => 
+                {
+                    updateDoc(ref, {option1: {name: data.option1.name, value: data.option1.value+=1}});
+                     goto('thanks');
+                }} id="op1">
+                    <p>{data.option1.name + data.option1.value}</p>
+                </button>
+                <button on:click={() => 
+                    {
+                        updateDoc(ref, {option2: {name: data.option2.name, value: data.option2.value+=1}});
+                         goto('thanks');
+                    }} id="op2">
+                    <p>{data.option2.name}</p>
+                </button>
+            </div>
+            {/if}
+        </div>
+    </Doc>
+
+</FirebaseApp>
+
+<style>
+    #page {
+        margin: -8px;
+        padding: 0;
+        color: #fafafa;
+        font-family: Arial, Helvetica, sans-serif;
+        height: 100vh;
+        width: 100vw;
+        background-color: salmon;
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    #title {
+        height: 20vh;
+        width: 100%;
+        text-align: center;
+    }
+
+    #title h1 {
+        padding-top: 1vh;
+        font-size: 4rem;
+    }
+
+    #page button {
+        height: 78vh;
+        width: 48vw;
+        border: none;
+        border-radius: 10px;
+        
+    }
+
+    #op1 {
+        background-color: mediumseagreen;
+    }
+
+    #op2 {
+        background-color: navy;
+    }
+
+    #page button p {
+        color:#fafafa;
+        font-size: 2rem;
+        font-weight: bolder;
+        text-shadow: 1px 1px 1px #aaa;
+        padding: 5px;
+    }
+</style>
